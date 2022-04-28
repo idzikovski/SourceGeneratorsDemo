1bb5ed58a03c77b3aeff6ae6444944a063eccb36
Ivan Djikovski <ivan.dzikovski@gmail.com>
2022-04-28 11:26:19 +0200

    Added MainPage and ViewModel

---
+++ SourceGenerators/SourceGenerators/ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SourceGenerators.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        private int _count;
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ClickMeCommand { get; set; }
+
+        public MainViewModel()
+        {
+            ClickMeCommand = new Command(ClickMeButtonClicked);
+        }
+
+        private void ClickMeButtonClicked()
+        {
+            Count++;
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #endregion
+    }
+}
